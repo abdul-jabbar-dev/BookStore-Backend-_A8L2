88f674c614625da3afb59be2f82015d639fe441b
Abdul Jabbar <abduljabber1532002@gmail.com>
2023-09-16 11:58:04 +0600

    addiing credential

---
+++ prisma/migrations/20230916055510_/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "password";
+
+-- CreateTable
+CREATE TABLE "Credential" (
+    "userId" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Credential_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Credential_userId_key" ON "Credential"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Credential" ADD CONSTRAINT "Credential_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
